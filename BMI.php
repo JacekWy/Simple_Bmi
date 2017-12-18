c9a666a455c3b70b4c101777da8b6d8063543a83
JacekWy <34398043+JacekWy@users.noreply.github.com>
2017-12-18 20:23:23 +0100

    isse

--- BMI.php
+++ BMI.php
@@ -17,31 +17,28 @@
 </html>
 
 <?php
+if(isset($_POST['licz'])) {
+    $waga = $_POST['waga'];
+    $wzrost = $_POST['wzrost'];
 
-$waga = $_POST['waga'];
-$wzrost = $_POST['wzrost'];
+    $wynik = $waga / pow($wzrost, 2);
 
-$wynik = $waga / pow($wzrost,2);
-echo round($wynik,2).'<br>';
+    echo round($wynik, 2) . '<br>';
 
 
-
-if($wynik < 16){
-    echo 'Wyglodzenie';
-}elseif ($wynik < 16.99)
-{
-    echo 'Wychudzenie';
-}elseif ($wynik < 18.48)
-{
-    echo 'Niedowaga';
-}elseif ($wynik <24.99)
-{
-    echo 'Waga Prawidlowa';
-}elseif ($wynik < 29.99)
-{
-    echo 'Nadwaga';
-}elseif ($wynik < 34.99){
-    echo '1 stopiem otylosci';
+    if ($wynik < 16) {
+        echo 'Wyglodzenie';
+    } elseif ($wynik < 16.99) {
+        echo 'Wychudzenie';
+    } elseif ($wynik < 18.48) {
+        echo 'Niedowaga';
+    } elseif ($wynik < 24.99) {
+        echo 'Waga Prawidlowa';
+    } elseif ($wynik < 29.99) {
+        echo 'Nadwaga';
+    } elseif ($wynik < 34.99) {
+        echo '1 stopiem otylosci';
+    }
 }
 
 
